b3302a429fb2b81425b05afe68795d727db16aab
Thabish <kadertabish2@gmail.com>
2022-11-21 20:20:22 +0400

    banner
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ components/Banner.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import banner1 from "../public/assets/amazonBanner1.jpg";
+import banner2 from "../public/assets/amazonBanner2.jpg";
+import banner3 from "../public/assets/amazonBanner3.jpg";
+import Image from "next/image";
+type Props = {};
+
+export const Banner = (props: Props) => {
+	return (
+		<section className="relative max-w-7xl mx-auto">
+			<Carousel
+				autoPlay
+				interval={5000}
+				transitionTime={1000}
+				showThumbs={false}
+				showIndicators={false}
+				infiniteLoop={true}
+			>
+				<Image loading="lazy" src={banner1} alt="/banner" />
+
+				<Image loading="lazy" src={banner2} alt="/banner" />
+
+				<Image loading="lazy" src={banner3} alt="/banner" />
+			</Carousel>
+			<div className="absolute inset-0 z-10 bg-gradient-to-t to-transparent from-gray-400"></div>
+		</section>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 
 export default function Home() {
@@ -9,6 +10,12 @@ export default function Home() {
 			</Head>
 			{/* Header */}
 			<Header />
+
+			<main>
+				{/* banner */}
+				<Banner />
+				{/* Feed */}
+			</main>
 		</div>
 	);
 }
