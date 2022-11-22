34bb746a8cbbffe2299368405e07a8103fe0d617
Thabish <kadertabish2@gmail.com>
2022-11-22 18:36:14 +0400

    ad-banner-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Products } from "../typings";
 import { Product } from "./Product";
-
+import Ad from "../public/assets/advertisement.jpg";
+import Image from "next/image";
 type Props = {
 	products: Products[];
 };
@@ -9,7 +10,13 @@ type Props = {
 export const Feed = ({ products }: Props) => {
 	return (
 		<div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-			{products.map((productData) => (
+			{products.slice(0, 4).map((productData) => (
+				<Product key={productData.id} products={productData} />
+			))}
+			<div className="mx-auto md:col-span-2 lg:col-span-3 xl:col-span-4">
+				<Image src={Ad} alt="ad" />
+			</div>
+			{products.slice(4, products.length).map((productData) => (
 				<Product key={productData.id} products={productData} />
 			))}
 		</div>
--- components/Product.tsx
+++ components/Product.tsx
@@ -11,7 +11,7 @@ export const Product = ({ products }: Props) => {
 	const randomRateing = new Array(Math.floor(Math.random() * 5) + 1);
 	const random_boolean = Math.random() < 0.5;
 	return (
-		<div className="flex flex-col m-3 p-10 bg-white">
+		<div className="flex flex-col m-3 p-10 bg-white ">
 			<p className="flex justify-end italic text-gray-400">
 				{products.category}
 			</p>
@@ -49,7 +49,7 @@ export const Product = ({ products }: Props) => {
 				)}
 			</div>
 
-			<button className="mt-auto button">Add to Basket</button>
+			<button className="mt-auto w-full button">Add to Basket</button>
 		</div>
 	);
 };
