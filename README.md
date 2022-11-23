cfae9df6f62be00269c0c25a6f18a81c506373f7
Thabish <kadertabish2@gmail.com>
2022-11-23 12:15:11 +0400

    cart
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 
 ### Resources
 
--[Removeing carousel Thumbnail](https://stackoverflow.com/questions/64358616/how-can-i-remove-the-part-below-the-carousel) -[The -Perils of Rehydration](https://www.joshwcomeau.com/react/the-perils-of-rehydration/)
+-[Removeing carousel Thumbnail](https://stackoverflow.com/questions/64358616/how-can-i-remove-the-part-below-the-carousel)
+
+-[The -Perils of Rehydration](https://www.joshwcomeau.com/react/the-perils-of-rehydration/)
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
 import React from "react";
 import { Header } from "../components/Header";
-
+import amazonAd from "../public/assets/amazonad.jpeg";
 type Props = {};
 
 export default function cart(props: Props) {
 	return (
-		<div>
+		<div className="bg-gray-200">
 			<Header />
 
-			<main className="max-w-screen-2xl mx-auto">
-				<img
-					className="w-full object-cover"
-					src="https://connect-assets.prosple.com/cdn/ff/zNryDlf-uf0qIh0kt7iOyMHP2B0NW30YDNDCOXMU-NM/1578558097/public/2020-01/Banner-Amazon-893x321-2020.jpg"
+			<main className="max-w-screen-2xl mx-auto  flex flex-col">
+				<Image
+					className="w-full max-h-[300px] object-cover"
+					src={amazonAd}
 					alt=""
 				/>
+				<h1 className="text-xl  my-2 border-b border-black">
+					Shopping Cart
+				</h1>
 			</main>
 		</div>
 	);
