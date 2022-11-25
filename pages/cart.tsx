86d8ba6f323f86299053bf1f0c07abf93c2dc42f
Thabish <kadertabish2@gmail.com>
2022-11-25 10:13:33 +0400

    right side added of cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -15,27 +15,39 @@ export default function cart(props: Props) {
 		<div className="bg-gray-200">
 			<Header />
 
-			<main className="max-w-screen-2xl mx-auto  flex flex-col">
-				<Image
-					className="w-full max-h-[300px] object-cover"
-					src={amazonAd}
-					alt=""
-				/>
-				<h1 className="text-xl  my-2 border-b border-black font-bold">
-					Shopping Cart
-				</h1>
-				{cartData.map((data) => (
-					<CartItem
-						key={data.id}
-						id={data.id}
-						title={data.title}
-						price={data.price}
-						description={data.description}
-						category={data.category}
-						image={data.image}
-						rating={data.rating}
+			<main className="max-w-screen-2xl mx-auto  grid gap-4 lg:grid-cols-5">
+				<div className="lg:col-span-4">
+					{/* banner */}
+					<Image
+						className="w-full max-h-[300px] object-cover"
+						src={amazonAd}
+						alt=""
 					/>
-				))}
+					<h1 className="text-xl  my-2 border-b border-black font-bold">
+						Shopping Cart
+					</h1>
+					{/* Items */}
+					{cartData.map((data) => (
+						<CartItem
+							key={data.id}
+							id={data.id}
+							title={data.title}
+							price={data.price}
+							description={data.description}
+							category={data.category}
+							image={data.image}
+							rating={data.rating}
+						/>
+					))}
+				</div>
+				{/* right side */}
+				<div className="lg:cols-span-1 bg-white p-4 flex flex-col m-3 lg:m-0">
+					<h1 className="text-lg whitespace-nowrap">
+						Total (Items : 5) :
+						<span className="font-bold">$1000</span>
+					</h1>
+					<button className="button">Proceed</button>
+				</div>
 			</main>
 		</div>
 	);
