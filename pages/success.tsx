5945e096cc26a7e1268041dc87e1f423c83cb661
Thabish <kadertabish2@gmail.com>
2022-11-27 15:18:00 +0400

    success-page
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ pages/success.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "../components/Header";
+
+type Props = {};
+
+const success = (props: Props) => {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+};
+
+export default success;
