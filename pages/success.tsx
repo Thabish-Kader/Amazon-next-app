da48526e6284321582247f4d654c0698b617faf6
Thabish <kadertabish2@gmail.com>
2022-11-27 15:27:35 +0400

    sucess-page-created
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Header } from "../components/Header";
-
+import { AiFillCheckCircle } from "react-icons/ai";
 type Props = {};
 
 const success = (props: Props) => {
 	return (
-		<div>
+		<div className="bg-gray-100 h-screen">
 			<Header />
+
+			<main className="max-w-screen-lg mx-auto mt-10">
+				<div className="flex flex-col p-10 bg-white">
+					<div className="flex items-center space-x-2 mb-5">
+						<AiFillCheckCircle
+							size={30}
+							className="text-green-500 h-10"
+						/>
+						<h1 className="text-3xl">
+							Thank you, your order has been placed
+						</h1>
+					</div>
+					<p>Your order will arrive to you soon</p>
+				</div>
+			</main>
 		</div>
 	);
 };
